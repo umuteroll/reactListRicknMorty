62f50c2947da2fbacfa675db93385d38f1c08126
Umut Erol <umut_erol1999@hotmail.com>
2022-12-22 23:39:16 +0300

    fix(Search): css  margin and flex

--- my-app/components/Search/Search.js
+++ my-app/components/Search/Search.js
@@ -3,12 +3,12 @@ import styles from "./Search.module.css";
 
 function Search() {
   return (
-    <div className={styles.container}>
+    <div className='container mt-5 mb-5'>
       <input
         id="rick-and-morty-search"
         type="search"
-        placeholder="Enter a character name or episode title"
-        className={styles.input}
+        placeholder="Enter a Location"
+        className= {`${styles.input} form-control form-control-lg shadow`  }
       />
     </div>
   );
