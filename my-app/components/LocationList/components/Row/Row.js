3897c978bbe407be2ec2b75279bcbd05566ff1b4
Umut Erol <umut_erol1999@hotmail.com>
2022-12-24 01:39:55 +0300

    feat: link changed to dynamic url

--- my-app/components/LocationList/components/Row/Row.js
+++ my-app/components/LocationList/components/Row/Row.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useContext,useState, useEffect } from 'react';
 import styles from "./Row.module.css";
 import Link from 'next/link';
+import RnMContext from "../../../../context/RnMContext";
 
 function Row({locationProp}) {
+  const { updateSelectedLocation } = useContext(RnMContext);
 
+  const handleClick = () => {
+    updateSelectedLocation(locationProp.id);
+  };
   return (
-    <div className="d-flex flex-row justify-content-between shadow border border-2 rounded m-2" >
+    <div className="d-flex flex-row justify-content-between shadow border border-2 rounded m-2 bg-light" >
       <div className="d-flex flex-row justify-content-start m-1">
         <img src="portalNew.png" className={styles.icon}></img>
         <div className="d-flex flex-column ms-3">
@@ -15,8 +21,8 @@ function Row({locationProp}) {
         </div>
       </div>
       <div className="d-flex align-items-center"> 
-      <Link href="/characters">
-      <input className="btn btn-primary shadow m-1 me-3 h-10" type="button" value="Detaya Git!"/>
+      <Link href="/location/[id]" as={'location/' + locationProp.id}>
+      <input className="btn btn-primary shadow m-1 me-3 h-10" type="button" value="Detaya Git!" onClick={handleClick}/>
       </Link>
       </div>
 
