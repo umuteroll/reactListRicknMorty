d58a00c5371f6bccc645ef69351730ff23e09c87
Umut Erol <umut_erol1999@hotmail.com>
2022-12-24 04:28:48 +0300

    fix: route changed

--- my-app/components/LocationList/components/Row/Row.js
+++ my-app/components/LocationList/components/Row/Row.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { useContext,useState, useEffect } from 'react';
 import styles from "./Row.module.css";
 import Link from 'next/link';
-import RnMContext from "../../../../context/RnMContext";
 
 function Row({locationProp}) {
-  const { updateSelectedLocation } = useContext(RnMContext);
 
-  const handleClick = () => {
-    updateSelectedLocation(locationProp.id);
-  };
+ 
   return (
     <div className="d-flex flex-row justify-content-between shadow border border-2 rounded m-2 bg-light" >
       <div className="d-flex flex-row justify-content-start m-1">
@@ -21,8 +16,8 @@ function Row({locationProp}) {
         </div>
       </div>
       <div className="d-flex align-items-center"> 
-      <Link href="/location/[id]" as={'location/' + locationProp.id}>
-      <input className="btn btn-primary shadow m-1 me-3 h-10" type="button" value="Detaya Git!" onClick={handleClick}/>
+      <Link href={'location/' + locationProp.id}>
+      <input className="btn btn-primary shadow m-1 me-3 h-10" type="button" value="Detaya Git!"/>
       </Link>
       </div>
 
