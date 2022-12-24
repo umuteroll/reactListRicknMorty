f77f48705c1a0c900c5dc80ec2de8d829e16f2ce
Umut Erol <umut_erol1999@hotmail.com>
2022-12-24 04:28:15 +0300

    feat: search improved for filter

--- my-app/components/Search/Search.js
+++ my-app/components/Search/Search.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React , { useState } from 'react';
 import styles from "./Search.module.css";
+
+const getFilteredResult = () => {
+  if(prop == 'Location'){
+    getFilteredLocationList(value);
+  }
+  else{
+    getFilteredCharacter(value);
+  }
+}
+const handleChange = (event) => {
+}
 
 function Search() {
   return (
-    <div className='container mt-5 mb-5'>
+    <div className='container mt-5 mb-5 d-flex flex-row'>
       <input
         id="rick-and-morty-search"
         type="search"
         placeholder="Enter a Location"
         className= {`${styles.input} form-control form-control-lg shadow`  }
+        onChange={handleChange}
       />
+      <input className="btn btn-primary shadow m-1 me-3 h-10" type="button" value="Bul" />
+
     </div>
   );
 }
