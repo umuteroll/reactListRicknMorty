b377b2714b82f2423ca3c7b53c63fbb3f8afcce7
Umut Erol <umut_erol1999@hotmail.com>
2022-12-24 04:27:33 +0300

    refactor: state name

--- my-app/context/RnMProvider.js
+++ my-app/context/RnMProvider.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
+import React,{ useState, useEffect } from 'react';
 import RnMContext from './RnMContext';
 import service    from './Service';
 import { getCharacterIdString } from '../helper/helper'
 
 const RnMProvider = ({ children }) => {
   const [state, setState] = useState({});
-  const [state2, setState2] = useState({});
+  const [characterState, setCharacterState] = useState({});
   const [loading, setLoading] = useState(true);
   const [location, setLocation] = useState();
 
@@ -24,17 +24,11 @@ const RnMProvider = ({ children }) => {
       }
      
     };
-    
-    const getCharacterList = async () => {
+    const getFilteredLocationList = async (locationName) => {
       setLoading(true);
       try{
-        // burası parametre olarak urldeki parametreyi alır location idsi 
-        const response = await service.get(`/location/${location}`);
-        const idList = getCharacterIdString(response.data.residents)
-        const res = await service.get(`/character/${idList}`);
-
-        //burada helper yardımıyla gelen arrayi 
-        setState2(res.data)
+        const response = await service.get(`/location/?name=${locationName}`);
+        setState(response.data.results);
       }
       catch{
        // error göster alert falan
@@ -44,14 +38,36 @@ const RnMProvider = ({ children }) => {
       }
      
     };
-    const updateSelectedLocation = (newLocation) => {
-      setLocation(newLocation);
-      setLoading(true);
-
+    
+    const getCharacterList = async (id) => {
+      if(!id){
+        console.log('id boş');
+        return;
+      }
+      try{
+        // burası parametre olarak urldeki parametreyi alır location idsi 
+        const response = await service.get(`/location/${id}`);
+        const idList = getCharacterIdString(response.data.residents)
+        if(idList==''){
+          setCharacterState({});
+          return;
+        }
+        const res = await service.get(`/character/${idList}`);
+        //burada helper yardımıyla gelen arrayi 
+        setCharacterState(res.data)
+      }
+      catch (err){
+       // error göster alert falan
+       console.log(err);
+      }
+      finally{
+      }
+     
     };
+  
 
   return (
-    <RnMContext.Provider value={{ state, state2, getLocationList, getCharacterList, updateSelectedLocation, loading }}>
+    <RnMContext.Provider value={{ state, characterState, getLocationList, getCharacterList, getFilteredLocationList,loading }}>
       {children}
     </RnMContext.Provider>
   );
